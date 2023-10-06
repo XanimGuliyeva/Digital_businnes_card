b7c7a115a05fd633ce77d1f33d4cbfa96e06a9f9
Xanim Guliyeva <57264503+XanimGuliyeva@users.noreply.github.com>
2023-10-06 23:15:55 +0400

    Add files via upload

--- components/Info.js
+++ components/Info.js
@@ -7,7 +7,7 @@ export default function Info() {
             <img src='../picture.jpg' alt="Your Name" />
             <h1 className='name'>Kahnim Guliyeva</h1>            
             <p className='position'>Junior FrontEnd Developer</p>
-            <a href="mailto:youremail@example.com" className="email-button">Email</a>
+            <a href="mailto:xanimguliyeva1@gmail.com" className="email-button">Email</a>
         </header>
     )
 }
